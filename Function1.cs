421fc9d83fecfde533a8cfb971fddef8d5210fef
Johnson Dubula <jdubula@iqbusiness.net>
2021-12-24 19:00:00 +0300

    Functions

--- Function1.cs
+++ Function1.cs
@@ -17,7 +17,7 @@ namespace TestAPI
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "{name}")] HttpRequest req,
             ILogger log,string name)
         {
-            return new OkObjectResult($"hell{name}");
+            return new OkObjectResult($"hello {name}");
         }
     }
 }
